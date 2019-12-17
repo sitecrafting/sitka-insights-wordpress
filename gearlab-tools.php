d3f8ad0f5660b2b428273192b839cc0e06596702
Coby Tamayo <ctamayo@sitecrafting.com>
2019-12-17 14:58:56 -0800

    fix autoloading and prevent running base WP file directly

--- gearlab-tools.php
+++ gearlab-tools.php
@@ -9,7 +9,18 @@
  * Version: 0.1.0
  */
 
-require_once __DIR__ . '/vendor/autoload.php';
+// no script kiddiez
+if (!defined('ABSPATH')) {
+  return;
+}
+
+// require the composer autoloader, making educated guesses as to where it is
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+  require_once __DIR__ . '/vendor/autoload.php';
+} elseif (file_exists(ABSPATH . '/vendor/autoload.php')) {
+  require_once ABSPATH . '/vendor/autoload.php';
+}
+
 require_once __DIR__ . '/wp-api.php';
 
 use GearLab\Api\Client;
