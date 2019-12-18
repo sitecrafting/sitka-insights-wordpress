c320f7ae55610507ba64ff8d905a5fc8bbbc7e99
Coby Tamayo <ctamayo@sitecrafting.com>
2019-12-17 16:02:56 -0800

    get phpunit running

---
+++ test/cases/PaginationTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Pagination tests
+ *
+ * @copyright 2020 SiteCrafting, Inc.
+ * @author    Coby Tamayo <ctamayo@sitecrafting.com>
+ */
+
+namespace GearLab;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test all the pagination things
+ */
+class PaginationTests extends TestCase {
+  public function test_pagination() {
+    $this->assertTrue(true);
+  }
+}
