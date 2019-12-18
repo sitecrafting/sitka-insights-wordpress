f7a32ca96148cc02654d24382c4bf0ea3440db47
Coby Tamayo <ctamayo@sitecrafting.com>
2019-12-17 20:48:11 -0800

    implement usable default search behavior

---
+++ views/pagination.php
@@ -0,0 +1,21 @@
+<?php if ($paginator->page_count() > 1) : ?>
+  <div class="pagination">
+    <?php foreach ($markers as $marker) : ?>
+      <?php if (!empty($marker['previous'])) : ?>
+        <a class="page-numbers prev" href="<?= $paginator->previous_page_url($params) ?>" rel="prev">Previous</a>
+      <?php endif; ?>
+
+      <?php if (!empty($marker['current'])) : ?>
+        <span aria-current="page" class="page-numbers current"><?= $marker['page_num'] ?></span>
+      <?php elseif (!empty($marker['filler'])) : ?>
+        <span class="page-numbers dots">…</span>
+      <?php elseif (!empty($marker['page_num'])) : ?>
+        <a class="page-numbers" href="<?= $marker['url'] ?>"><?= $marker['page_num'] ?></a>
+      <?php endif ?>
+
+      <?php if (!empty($marker['next'])) : ?>
+        <a class="page-numbers next" href="<?= $paginator->next_page_url($params) ?>" rel="next">Next</a>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </div>
+<?php endif; ?>
--- wp-api.php
+++ wp-api.php
@@ -9,14 +9,62 @@
 
 namespace GearLab;
 
+use GearLab\Plugin\Paginator;
+
 function client() {
   return apply_filters('gearlab/api/client', null);
 }
 
-function search(array $params) : array {
-  return client()->search($params);
+function search(array $params = []) : array {
+  return client()->search(apply_filters('gearlab/search/params', $params));
 }
 
 function completions(array $params) : array {
   return client()->completions($params);
 }
+
+function paginate_links(array $response) : string {
+  $paginator = Paginator::from_search_response($response);
+  // TODO filters for pagination params
+  $markers = $paginator->page_markers($_GET);
+  $params = $_GET;
+
+  ob_start();
+  require __DIR__ . '/views/pagination.php';
+  return ob_get_clean();
+}
+
+
+add_filter('gearlab/search/params', function(array $params) : array {
+  if ($params) {
+    // we already have params; no need to set up defaults
+    return $params;
+  }
+
+  $pageNum = apply_filters('gearlab/search/page_num', 1);
+  $count   = apply_filters('gearlab/search/result_count', 10);
+  $offset  = ($pageNum - 1) * $count;
+
+  return [
+    'query'     => apply_filters('gearlab/search/query', $_GET['s'] ?? ''),
+    'resLength' => $count,
+    'resOffset' => $offset,
+    'metaTag'   => apply_filters('gearlab/search/meta_tag', ''),
+  ];
+}, 10);
+
+add_filter('gearlab/search/result_count', function() : int {
+  static $count;
+  if (!isset($count)) {
+    $count = (int) get_option('posts_per_page');
+  }
+  return $count;
+}, 1);
+
+add_filter('gearlab/search/page_num', function() : int {
+  return $_GET['page_num'] ?? 1;
+}, 1);
+
+add_filter('gearlab/search/meta_tag', function() : string {
+  return '';
+}, 1);
