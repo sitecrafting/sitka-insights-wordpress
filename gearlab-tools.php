f8f305eee1b1f136f494399a9ebf9d5833645a3b
Coby Tamayo <ctamayo@sitecrafting.com>
2020-01-31 16:14:20 -0800

    bump version to v0.2.3

--- gearlab-tools.php
+++ gearlab-tools.php
@@ -6,7 +6,7 @@
  * Plugin URI: https://gearlab.tools
  * Author: Coby Tamayo <ctamayo@sitecrafting.com>
  * Author URI: https://www.sitecrafting.com/
- * Version: 0.2.2
+ * Version: 0.2.3
  */
 
 // no script kiddiez
