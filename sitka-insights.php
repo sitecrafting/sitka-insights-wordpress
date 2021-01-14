b4ed4135fc2728589901be23dfe89c27bfb669d4
Phil Price <pprice@Phils-MacBook-Pro.local>
2021-01-14 12:26:32 -0800

    Incremented version number

--- sitka-insights.php
+++ sitka-insights.php
@@ -6,7 +6,7 @@
  * Plugin URI: https://www.sitkainsights.com
  * Author: SiteCrafting, Inc. <hello@sitecrafting.com>
  * Author URI: https://www.sitecrafting.com/
- * Version: 2.3.0
+ * Version: 2.3.3
  * Requires PHP: 7.1
  */
 
