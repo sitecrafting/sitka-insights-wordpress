9dfc52196c5c88aec0c86495171f99a859bd7081
Andrzej Koziol <akoziol@sitecrafting.com>
2024-10-29 15:30:51 -0700

    bumped version to v3.0.0, update composer file to move to sitecrafting/gearlab-tools-php v3.0.0

--- sitka-insights.php
+++ sitka-insights.php
@@ -6,7 +6,7 @@
  * Plugin URI: https://www.sitkainsights.com
  * Author: SiteCrafting, Inc. <hello@sitecrafting.com>
  * Author URI: https://www.sitecrafting.com/
- * Version: 2.3.3
+ * Version: 3.0.0
  * Requires PHP: 7.1
  */
 
