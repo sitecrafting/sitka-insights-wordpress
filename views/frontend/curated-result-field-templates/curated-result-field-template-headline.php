92d1ef2dc7a660907975da91b6e170dc42402396
Andrzej Koziol <akoziol@sitecrafting.com>
2024-10-29 09:35:41 -0700

    fixed variable name in curated-result-field-template-headline.php file

--- views/frontend/curated-result-field-templates/curated-result-field-template-headline.php
+++ views/frontend/curated-result-field-templates/curated-result-field-template-headline.php
@@ -4,10 +4,10 @@ $field = $data['curated_result_field'] ?? [];
 
 ?>
 
-<?php foreach ($field['value'] as $field_link) : ?>
+<?php foreach ($field['value'] as $field_value) : ?>
   
   <?php if ($field_value['headline'] ?? false && !empty($field_value['headline'])) : ?>
-    <h3 class="curated-result-<?= $field['type'] ?>"><?= $field_link['headline'] ?></h3>
+    <h3 class="curated-result-<?= $field['type'] ?>"><?= $field_value['headline'] ?></h3>
   <?php endif; ?>
 
 <?php endforeach; ?>
