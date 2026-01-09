163fa35c4040875249c83ec8d9a017fdc1dbc348
Sean Dean <sdean@sitecrafting.com>
2026-01-09 10:18:23 -0800

    bash linting and archive test

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -126,10 +126,10 @@ function restore_vendor() {
 
 function create_github_release() {
   if [[ $(which hub) ]] ; then
-    echo $($BOLD)hub detected! You win at Git!$($RESET)
-    read -p 'Create a GitHub release? (y/N) ' create
+    echo "$($BOLD)hub detected! You win at Git!$($RESET)"
+    read -r -p 'Create a GitHub release? (y/N) ' create
     if [[ "$create" = "y" ]] ; then
-      read -p 'Is this a pre-release? (y/N) ' prerelease
+      read -r -p 'Is this a pre-release? (y/N) ' prerelease
       if [[ "$prerelease" = "y" ]] ; then
         prerelease_opt='--prerelease'
       fi
@@ -137,7 +137,7 @@ function create_github_release() {
       echo 'pushing latest changes and tags...'
       git push origin main
       git push --tags
-      hub release create $prerelease_opt -a "$2" -a "$3" -e "$1"
+      hub release create "$prerelease_opt" -a "$2" -a "$3" -e "$1"
     else
       echo 'skipping GitHub release.'
     fi
