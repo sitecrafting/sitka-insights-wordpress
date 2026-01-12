385896576126b7cbb360fa08d5ef9493cbd1f16e
Sean Dean <sdean@sitecrafting.com>
2026-01-12 07:14:40 -0800

    Bump version to 0.0.8b

--- sitka-insights.php
+++ sitka-insights.php
@@ -6,7 +6,7 @@
  * Plugin URI: https://www.sitkainsights.com
  * Author: SiteCrafting, Inc. <hello@sitecrafting.com>
  * Author URI: https://www.sitecrafting.com/
- * Version: 0.0.7b
+ * Version: 0.0.8b
  * Requires PHP: 7.1
  */
 
