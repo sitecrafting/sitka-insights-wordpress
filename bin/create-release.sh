d4a4e8d51eb2dcd801065ddbc5dba5f6ec8277c8
pprice <pprice@sitecrafting.com>
2026-09-22 10:29:04 -0700

    Repaired regex check in create-release.sh

--- bin/create-release.sh
+++ bin/create-release.sh
@@ -58,8 +58,9 @@ echo ""
 read -p "Enter new version number (e.g., 1.2.3): " VERSION
 
 # Validate version format (basic semver check)
-if ! [[ "$VERSION" =~ ^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$
- ]]; then
+SEMVER_REGEX='^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$'
+
+if ! [[ "$VERSION" =~ $SEMVER_REGEX ]]; then
     error "Invalid version format. Please use semantic versioning (e.g., 1.2.3)"
 fi
 
